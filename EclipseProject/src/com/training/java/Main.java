ffae5ce9723c7e95611600a7ec0a90b4350496da
Jeffrey Miller <miller.s.jeffrey@gmail.com>
2017-06-14 22:30:19 -0500

    too much shit going on at home... Small commit

---
+++ EclipseProject/src/com/training/java/Main.java
@@ -0,0 +1,13 @@
+package com.training.java;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello from Eclipse!");
+		String aString = "Jeff";
+		System.out.println("Your name is  " + aString);
+		
+
+	}
+
+}
