775f598d5ebe2c8732736d0fecf3e48195b1a8df
Jeffrey Miller <miller.s.jeffrey@gmail.com>
2017-06-23 22:52:22 -0500

    A simple addition program that adds to values together after parsing the original strings into numbers, then into doubles.

---
+++ EclipseProject/src/com/training/java/challenges/SimpleCalculator.java
@@ -0,0 +1,26 @@
+package com.training.java.challenges;
+
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Scanner;
+
+public class SimpleCalculator {
+
+	public static void main(String[] args) throws ParseException {
+		
+		Scanner scanner = new Scanner(System.in);
+		String firstValue, secondValue;
+		System.out.println("What is the first value you'd like to use for our addition problem?");
+		firstValue = scanner.nextLine();
+		System.out.println("And what will we use as the 2nd number used to find the sum?");
+		secondValue = scanner.nextLine();		
+		
+		Number f1 = NumberFormat.getInstance().parse(firstValue);
+		Number f2 = NumberFormat.getInstance().parse(secondValue);
+		double d1 = f1.doubleValue();
+		double d2 = f2.doubleValue();
+
+		System.out.println("The sum of the values is: " + (d1 + d2));
+	}
+
+}
