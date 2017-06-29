0a7307d76581965d238f84de1376b10d7ef6fbf5
Jeffrey Miller <miller.s.jeffrey@gmail.com>
2017-06-28 21:46:51 -0500

    introduction to debugging

---
+++ EclipseProject/src/com/training/java/ErrorsAndExceptions/Debugging.java
@@ -0,0 +1,15 @@
+package com.training.java.ErrorsAndExceptions;
+
+public class Debugging {
+
+	public static void main(String[] args) {
+
+		String welcome = "Welcome!";
+		char[] chars = welcome.toCharArray();
+		
+		for (int i = 0; i < chars.length; i++0
+			System.out.println(chars[i]);
+		
+	}
+
+}
