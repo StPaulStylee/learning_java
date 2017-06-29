92ea739889c67247aebca78b4576c5967da64124
Jeffrey Miller <miller.s.jeffrey@gmail.com>
2017-06-28 22:04:36 -0500

    creating custom exceptions

--- EclipseProject/src/com/training/java/ErrorsAndExceptions/ErrorsAndExceptions.java
+++ EclipseProject/src/com/training/java/ErrorsAndExceptions/ErrorsAndExceptions.java
@@ -13,6 +13,9 @@ public class ErrorsAndExceptions {
 
 		
 		try {
+			if (chars.length < 10) {
+				throw (new Exception("My custom exception, bitches!"));
+			}
 			lastChar = chars[chars.length - 1];
 			System.out.println(lastChar);
 			
@@ -23,6 +26,8 @@ public class ErrorsAndExceptions {
 			System.out.println("Ya gone fucked up!");
 		} catch (StringIndexOutOfBoundsException e) {
 			System.out.println("Why you gotta try and make a substring all incorrectly!?");
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
 		}
 		
 	}
