cc04b6de31d46350ae76183e1959d20b8fa3a27e
Jeffrey Miller <miller.s.jeffrey@gmail.com>
2017-06-29 22:32:15 -0500

    forEach() loop syntax for java

---
+++ EclipseProject/src/com/training/java/ProgramFlow/ForEach.java
@@ -0,0 +1,14 @@
+package com.training.java.ProgramFlow;
+
+public class ForEach {
+
+	public static void main(String[] args) {
+
+		String[] months = {"Jan", "Feb", "Mar", "Apr", "May" };
+		
+		for (String month : months) {
+			System.out.println(month);
+		}
+	}
+
+}
