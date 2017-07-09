38a63dee372ef5ea266b110525a02edb54cd4949
Jeffrey Miller <miller.s.jeffrey@gmail.com>
2017-07-08 19:07:15 -0500

    working with java constructors - pretty gd simple

--- EclipseProject/src/com/training/java/InstanceVariables/Main.java
+++ EclipseProject/src/com/training/java/InstanceVariables/Main.java
@@ -8,14 +8,9 @@ public class Main {
 	public static void main(String[] args) {
 
 		List<Olives> olives = new ArrayList<>();
-		Olives olive1 = new Olives();
-		olives.add(olive1);
-		Olives olive2 = new Olives();
-		olive2.setOil(5);
-		olives.add(olive2);
-		Olives olive3 = new Olives();
-		olive3.setOil(2);
-		olives.add(olive3);
+		olives.add(new Olives("Kalamata", 0x2E0854, 2));
+		olives.add(new Olives("Kalamata", 0x2E0854, 4));
+		olives.add(new Olives("Black", 0x000000, 2));
 		
 		OlivePress press = new OlivePress();
 		int totalOil = press.getOil(olives);
